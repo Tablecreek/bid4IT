81b2c067f6d53adbe0ac85edbb8599cb5fff872e
Alain Tellenbach <tablecreek@gmail.com>
2015-10-21 13:22:56 +0200

    LoginPage which extends BasePage was added

---
+++ bid4IT/Views/BasePage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace bid4IT.Views {
+    public sealed partial class BasePage : Page {
+        public BasePage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ bid4IT/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace bid4IT.Views {
+    public sealed partial class LoginPage {
+        public LoginPage() {
+            InitializeComponent();
+        }
+    }
+}

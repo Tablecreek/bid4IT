9de06da75bfb3d5e20c357e37894f4e4b7a86c89
Alain Tellenbach <tablecreek@gmail.com>
2015-10-21 13:19:50 +0200

    Initial commit

---
+++ bid4IT/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace bid4IT {
+    sealed partial class App {
+        public App() {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(Microsoft.ApplicationInsights.WindowsCollectors.Metadata | Microsoft.ApplicationInsights.WindowsCollectors.Session);
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+        
+        protected override void OnLaunched(LaunchActivatedEventArgs e) {
+           var rootFrame = Window.Current.Content as Frame;
+            
+            if (rootFrame == null) {
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated) {
+                    //TODO: Load state from previously suspended application
+                }
+                
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null) {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            Window.Current.Activate();
+        }
+        
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e) {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+        
+        private void OnSuspending(object sender, SuspendingEventArgs e) {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
